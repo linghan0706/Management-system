61e01b133eb67707e25ba13f2759835d5da7bb4d
linghan0706 <159553534+linghan0706@users.noreply.github.com>
2025-03-22 02:03:54 +0800

    修改第一次

---
+++ src/views/system/menu/menu-list.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="menu-list-container">
+    <!-- 菜单列表内容将在这里实现 -->
+  </div>
+</template>
+
+<script setup lang="ts">
+// 导入所需的组件和API
+
+</script>
+
+<style scoped>
+.menu-list-container {
+  padding: 16px;
+}
+</style>
